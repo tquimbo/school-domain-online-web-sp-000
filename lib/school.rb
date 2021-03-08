18502138c9e55a6347108322cd06eb0e0db130e0
Tim Quimbo <tquimbo1@gmail.com>
2021-03-08 22:55:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+attr_accessor :school, :roster, :student, :grade
+
+
+
+def initialize(roster)
+  @roster= {}
+end
+
+def add_student(student, grade)
+roster[grade] ||= []
+roster[grade] << student
+end
+
+def grade(grade)
+return roster[grade]
+end
+
+def sort
+  sort = {}
+  roster.each do |grade, students|
+      sort[grade] = students.sort
+    end
+return sort
+end
+
+end
